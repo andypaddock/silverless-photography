2994da7604acfe46c82e27ab18c4f22c979c553b
Andy Paddock <appaddock@gmail.com>
2020-08-12 11:41:08 +0100

    Update our-work.php

--- page-templates/our-work.php
+++ page-templates/our-work.php
@@ -20,7 +20,46 @@ get_header();?>
         </div>
     </div>
 </div>
+<div class="outer-container leaders">
 
+    <div class="container">
+        <?php
+                    $currentID = get_the_ID();
+                    $loop = new WP_Query(
+                        array(
+                            'post_type' => 'work',
+                            'posts_per_page' => -1,
+                            'post__not_in' => array($currentID),
+                        )
+                    );
+                    while ( $loop->have_posts() ) : $loop->the_post();
+                    $leaderImage = get_field('thumbnail_image', $post->ID);
+                    $text = get_field('copy', $post->ID);
+                    if ( '' != $text ) {
+                        $text = strip_shortcodes( $text );
+                        $text = apply_filters('the_content', $text);
+                        $text = str_replace(']]>', ']]>', $text);
+                        $excerpt_length = 22; // 20 words
+                        $excerpt_more = apply_filters('excerpt_more', '' . '...');
+                        $text = wp_trim_words( $text, $excerpt_length, $excerpt_more );
+                    }?>
+
+        <div class="copy">
+            <h3 class="heading heading__7"><?php the_title(); ?></h4>
+                <p><?php echo $text; ?></p>
+                <span class="dark-highlight"></span>
+                <a href="<?php the_permalink(); ?>" class="button button__arrowright"
+                    alt="silverless studio | <?php the_title(); ?>">Read
+                    More<?php get_template_part("inc/img/arrow");?></a>
+        </div>
+        <div class="image" style="background:url(<?php echo $leaderImage['url'];?>);"></div>
+
+        <?php endwhile;
+                    wp_reset_postdata();
+                    ?>
+    </div>
+
+</div>
 <div class="outer-container leaders">
     <?php if( have_rows('leader_section') ):
 			while( have_rows('leader_section') ): the_row();
